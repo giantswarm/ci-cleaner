bfbd91c757ae326d38e9381296a4d90efbec0840
Tim Schindler <xh3b4sd@gmail.com>
2018-07-10 11:57:34 +0200

    cleans up vnet peering leftovers from CI (#8)

--- pkg/cleaner/azure/cleaner.go
+++ pkg/cleaner/azure/cleaner.go
@@ -3,13 +3,17 @@ package azure
 import (
 	"context"
 	"fmt"
+	"strings"
+	"time"
 
 	"github.com/Azure/azure-sdk-for-go/services/network/mgmt/2018-05-01/network"
+	"github.com/Azure/azure-sdk-for-go/services/resources/mgmt/2018-02-01/resources"
 	"github.com/giantswarm/microerror"
 	"github.com/giantswarm/micrologger"
 )
 
 type CleanerConfig struct {
+	GroupsClient                 *resources.GroupsClient
 	Logger                       micrologger.Logger
 	VirtualNetworkPeeringsClient *network.VirtualNetworkPeeringsClient
 	VirtualNetworksClient        *network.VirtualNetworksClient
@@ -18,6 +22,7 @@ type CleanerConfig struct {
 }
 
 type Cleaner struct {
+	groupsClient                 *resources.GroupsClient
 	logger                       micrologger.Logger
 	virtualNetworkPeeringsClient *network.VirtualNetworkPeeringsClient
 	virtualNetworksClient        *network.VirtualNetworksClient
@@ -26,6 +31,9 @@ type Cleaner struct {
 }
 
 func NewCleaner(config CleanerConfig) (*Cleaner, error) {
+	if config.GroupsClient == nil {
+		return nil, microerror.Maskf(invalidConfigError, "%T.GroupsClient must not be empty", config)
+	}
 	if config.Logger == nil {
 		return nil, microerror.Maskf(invalidConfigError, "%T.Logger must not be empty", config)
 	}
@@ -44,7 +52,8 @@ func NewCleaner(config CleanerConfig) (*Cleaner, error) {
 	}
 
 	c := &Cleaner{
-		logger: config.Logger,
+		groupsClient: config.GroupsClient,
+		logger:       config.Logger,
 		virtualNetworkPeeringsClient: config.VirtualNetworkPeeringsClient,
 		virtualNetworksClient:        config.VirtualNetworksClient,
 
@@ -55,39 +64,59 @@ func NewCleaner(config CleanerConfig) (*Cleaner, error) {
 }
 
 func (c *Cleaner) Clean(ctx context.Context) error {
-	// TODO cleanup peeringso
-	//
-	//     https://godoc.org/github.com/Azure/azure-sdk-for-go/services/network/mgmt/2018-05-01/network#VirtualNetworksClient
-	//     https://godoc.org/github.com/Azure/azure-sdk-for-go/services/network/mgmt/2018-05-01/network#VirtualNetworkPeeringsClient
-	//
+	c.logger.LogCtx(ctx, "level", "debug", "message", "starting Azure CI cleanup")
 
-	r, err := c.virtualNetworksClient.ListAll(ctx)
-	if err != nil {
-		return microerror.Mask(err)
-	}
-
-	for _, v := range r.Values() {
-		// We skip all virtual networks of installations we do not want to cleanup.
-		if !containsString(c.installations, *v.Name) {
-			continue
+	for _, i := range c.installations {
+		r, err := c.virtualNetworksClient.List(ctx, i)
+		if err != nil {
+			return microerror.Mask(err)
 		}
-		fmt.Printf("\n")
-		fmt.Printf("%#v\n", *v.ID)
-		fmt.Printf("%#v\n", *v.Name)
-		fmt.Printf("\n")
+
+		for {
+			for _, v := range r.Values() {
+				for _, p := range *v.VirtualNetworkPeerings {
+					if !isCIResource(*p.Name) {
+						continue
+					}
+
+					_, err = c.groupsClient.Get(ctx, *p.Name)
+					if IsResourceGroupNotFound(err) && p.PeeringState == network.VirtualNetworkPeeringStateDisconnected {
+						c.logger.LogCtx(ctx, "level", "debug", "message", fmt.Sprintf("deleting vnet peering '%s'", *p.Name))
+
+						_, err := c.virtualNetworkPeeringsClient.Delete(ctx, i, *v.Name, *p.Name)
+						if err != nil {
+							return microerror.Mask(err)
+						}
+
+						c.logger.LogCtx(ctx, "level", "debug", "message", fmt.Sprintf("deleted vnet peering '%s'", *p.Name))
+
+						time.Sleep(1 * time.Second)
+						continue
+					} else if err != nil {
+						return microerror.Mask(err)
+					}
+				}
+			}
+
+			if r.NotDone() {
+				err = r.Next()
+				if err != nil {
+					return microerror.Mask(err)
+				}
+				continue
+			}
+
+			break
+		}
 	}
+
+	c.logger.LogCtx(ctx, "level", "debug", "message", "finished Azure CI cleanup")
 
 	return nil
 }
 
-func containsString(list []string, s string) bool {
-	for _, l := range list {
-		if l == s {
-			return true
-		}
-	}
-
-	return false
+func isCIResource(s string) bool {
+	return strings.HasPrefix(s, "ci-cur-") || strings.HasPrefix(s, "ci-wip-")
 }
 
 func isAnyEmpty(list []string) bool {
--- pkg/cleaner/azure/error.go
+++ pkg/cleaner/azure/error.go
@@ -1,8 +1,35 @@
 package azure
 
 import (
+	"github.com/Azure/go-autorest/autorest"
 	"github.com/giantswarm/microerror"
 )
+
+var resourceGroupNotFoundError = microerror.New("resource group not found")
+
+// IsResourceGroupNotFound asserts resourceGroupNotFoundError.
+func IsResourceGroupNotFound(err error) bool {
+	if err == nil {
+		return false
+	}
+
+	c := microerror.Cause(err)
+
+	if c == resourceGroupNotFoundError {
+		return true
+	}
+
+	{
+		dErr, ok := c.(autorest.DetailedError)
+		if ok {
+			if dErr.StatusCode == 404 {
+				return true
+			}
+		}
+	}
+
+	return false
+}
 
 var invalidConfigError = microerror.New("invalid config")
 
