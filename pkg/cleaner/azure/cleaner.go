82d1725cf16627048df55ba6cd7d3d138642903d
Theo Brigitte <theo.brigitte@gmail.com>
2018-08-17 15:30:27 +0200

    move azure vnetpeering cleanup to its own function and file

--- pkg/cleaner/azure/cleaner.go
+++ pkg/cleaner/azure/cleaner.go
@@ -2,9 +2,7 @@ package azure
 
 import (
 	"context"
-	"fmt"
 	"strings"
-	"time"
 
 	"github.com/Azure/azure-sdk-for-go/services/network/mgmt/2018-05-01/network"
 	"github.com/Azure/azure-sdk-for-go/services/resources/mgmt/2018-02-01/resources"
@@ -66,48 +64,9 @@ func NewCleaner(config CleanerConfig) (*Cleaner, error) {
 func (c *Cleaner) Clean(ctx context.Context) error {
 	c.logger.LogCtx(ctx, "level", "debug", "message", "starting Azure CI cleanup")
 
-	for _, i := range c.installations {
-		r, err := c.virtualNetworksClient.List(ctx, i)
-		if err != nil {
-			return microerror.Mask(err)
-		}
-
-		for {
-			for _, v := range r.Values() {
-				for _, p := range *v.VirtualNetworkPeerings {
-					if !isCIResource(*p.Name) {
-						continue
-					}
-
-					_, err = c.groupsClient.Get(ctx, *p.Name)
-					if IsResourceGroupNotFound(err) && p.PeeringState == network.VirtualNetworkPeeringStateDisconnected {
-						c.logger.LogCtx(ctx, "level", "debug", "message", fmt.Sprintf("deleting vnet peering '%s'", *p.Name))
-
-						_, err := c.virtualNetworkPeeringsClient.Delete(ctx, i, *v.Name, *p.Name)
-						if err != nil {
-							return microerror.Mask(err)
-						}
-
-						c.logger.LogCtx(ctx, "level", "debug", "message", fmt.Sprintf("deleted vnet peering '%s'", *p.Name))
-
-						time.Sleep(1 * time.Second)
-						continue
-					} else if err != nil {
-						return microerror.Mask(err)
-					}
-				}
-			}
-
-			if r.NotDone() {
-				err = r.Next()
-				if err != nil {
-					return microerror.Mask(err)
-				}
-				continue
-			}
-
-			break
-		}
+	err := c.CleanVirtualNetworkPeering(ctx)
+	if err != nil {
+		return microerror.Mask(err)
 	}
 
 	c.logger.LogCtx(ctx, "level", "debug", "message", "finished Azure CI cleanup")
---
+++ pkg/cleaner/azure/vnetpeering.go
@@ -0,0 +1,60 @@
+package azure
+
+import (
+	"context"
+	"fmt"
+	"time"
+
+	"github.com/Azure/azure-sdk-for-go/services/network/mgmt/2018-05-01/network"
+	"github.com/giantswarm/microerror"
+)
+
+// CleanVirtualNetworkPeering delete virtual network peering
+// leftover by e2e test on control plane.
+func (c Cleaner) CleanVirtualNetworkPeering(ctx context.Context) error {
+	for _, i := range c.installations {
+		r, err := c.virtualNetworksClient.List(ctx, i)
+		if err != nil {
+			return microerror.Mask(err)
+		}
+
+		for {
+			for _, v := range r.Values() {
+				for _, p := range *v.VirtualNetworkPeerings {
+					if !isCIResource(*p.Name) {
+						continue
+					}
+
+					_, err = c.groupsClient.Get(ctx, *p.Name)
+					if IsResourceGroupNotFound(err) && p.PeeringState == network.VirtualNetworkPeeringStateDisconnected {
+						c.logger.LogCtx(ctx, "level", "debug", "message", fmt.Sprintf("deleting vnet peering '%s'", *p.Name))
+
+						_, err := c.virtualNetworkPeeringsClient.Delete(ctx, i, *v.Name, *p.Name)
+						if err != nil {
+							return microerror.Mask(err)
+						}
+
+						c.logger.LogCtx(ctx, "level", "debug", "message", fmt.Sprintf("deleted vnet peering '%s'", *p.Name))
+
+						time.Sleep(1 * time.Second)
+						continue
+					} else if err != nil {
+						return microerror.Mask(err)
+					}
+				}
+			}
+
+			if r.NotDone() {
+				err = r.Next()
+				if err != nil {
+					return microerror.Mask(err)
+				}
+				continue
+			}
+
+			break
+		}
+	}
+
+	return nil
+}
